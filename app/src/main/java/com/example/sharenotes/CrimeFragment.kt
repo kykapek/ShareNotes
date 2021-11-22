957bacbd4daf65e52489fbc336a1ab9caed9132b
kykapek <alexeyalkiselev@gmail.com>
2021-11-23 01:28:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sharenotes/CrimeFragment.kt
@@ -0,0 +1,80 @@
+package com.example.sharenotes
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_crime.*
+
+class CrimeFragmet : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.editTextCrimeTitle) as EditText
+        dateButton = view.findViewById(R.id.btnCrimeDate) as Button
+        solvedCheckBox = view.findViewById(R.id.checkBoxCrimeSolved) as CheckBox
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object: TextWatcher{
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?, //ввод пользователя, вернет строку
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                crime.title = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener{ _, isChecked->
+                crime.isSolved = isChecked
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sharenotes/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.sharenotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment = CrimeFragmet();
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+}
